63acb3b2d50f00e7dfe7c354416cfa9b86c62e51
TyCanSing-ctrl <ivan3582838@gmail.com>
2025-06-21 15:28:58 +0800

    Update Offsets.hpp

--- Miscellaneous/NeedUpdating/Offsets.hpp
+++ Miscellaneous/NeedUpdating/Offsets.hpp
@@ -11,47 +11,50 @@ struct lua_State;
 struct Proto;
 
 namespace Offsets {
-    const uintptr_t OpcodeLookupTable = REBASE(0x55d4eb0);
-    const uintptr_t Print = REBASE(0x1563AC0);
-    const uintptr_t VariantCastInt64 = REBASE(0x14F40B0);
-    const uintptr_t RawScheduler = REBASE(0x681CA18);
-    const uintptr_t GetGlobalStateForInstance = REBASE(0xDBD570);
-    const uintptr_t DecryptState = REBASE(0xB44700);
-    const uintptr_t LuaVMLoad = REBASE(0xB47780);
-    const uintptr_t RequestCode = REBASE(0x907750);
-    const uintptr_t TaskSchedulerTargetFps = REBASE(0x632D050);
+    const uintptr_t OpcodeLookupTable = REBASE(0x562a4f0);
+    const uintptr_t Print = REBASE(0x15977E0);
+    const uintptr_t VariantCastInt64 = REBASE(0x1526730);
+    const uintptr_t RawScheduler = REBASE(0x6884DC8);
+    const uintptr_t GetGlobalStateForInstance = REBASE(0xDC7E20);
+    const uintptr_t DecryptState = REBASE(0xB4D320);
+    const uintptr_t LuaVMLoad = REBASE(0xB503A0);
+    const uintptr_t RequestCode = REBASE(0x906AC0);
+    const uintptr_t TaskSchedulerTargetFps = REBASE(0x6394020);
 
-    const uintptr_t Impersonator = REBASE(0x3403E90);
-    const uintptr_t PushInstance = REBASE(0xE9A590);
-    const uintptr_t PushInstance2 = REBASE(0xE9A5E0);
-    const uintptr_t Luau_Execute = REBASE(0x2774A30);
-    const uintptr_t TaskDefer = REBASE(0xFE5D80);
-    const uintptr_t Task__Spawn = REBASE(0xFE60A0);
-    const uintptr_t LuaD_throw = REBASE(0x2741980);
-    const uintptr_t LuaO_NilObject = REBASE(0x46ED098);
-    const uintptr_t LuaH_DummyNode = REBASE(0x46ECAB8);
-    const uintptr_t KTable = REBASE(0x632D0D0);
+    const uintptr_t Impersonator = REBASE(0x335F140);
+    const uintptr_t PushInstance      = REBASE(0xEA2D70);
+    const uintptr_t PushInstance2     = REBASE(0xEA2DC0);
+    const uintptr_t Luau_Execute      = REBASE(0x26D4300);
+    const uintptr_t TaskDefer         = REBASE(0xFEFA00);
+    const uintptr_t Task__Spawn       = REBASE(0xFEFD20);
+    const uintptr_t LuaD_throw        = REBASE(0x26A1550);
+    const uintptr_t LuaO_NilObject    = REBASE(0x4740458);
+    const uintptr_t LuaH_DummyNode    = REBASE(0x473FB88);
+    const uintptr_t KTable            = REBASE(0x63940A0);
 
-    const uintptr_t FireMouseClick = REBASE(0x1C62AA0);
-    const uintptr_t FireRightMouseClick = REBASE(0x1C62C40);
-    const uintptr_t FireMouseHoverEnter = REBASE(0x1C64040);
-    const uintptr_t FireMouseHoverLeave = REBASE(0x1C641E0);
 
-    const uintptr_t FireTouchInterest = REBASE(0x14366F0);
-    const uintptr_t GetIdentityStruct = REBASE(0x38073C0);
-    const uintptr_t IdentityPtr = REBASE(0x6363398);
-    const uintptr_t GetProperty = REBASE(0xA59750);
-    const uintptr_t FireProximityPrompt = REBASE(0x1D2E250);
+    const uintptr_t FireMouseClick         = REBASE(0x1CA01D0);
+    const uintptr_t FireRightMouseClick    = REBASE(0x1CA0370);
+    const uintptr_t FireMouseHoverEnter    = REBASE(0x1CA1770);
+    const uintptr_t FireMouseHoverLeave    = REBASE(0x1CA1910);
 
+    const uintptr_t FireTouchInterest      = REBASE(0x145F4D0);
+    const uintptr_t GetIdentityStruct      = REBASE(0x38530A0);
+    const uintptr_t IdentityPtr            = REBASE(0x63C9D48);
+    const uintptr_t GetProperty            = REBASE(0xA5C7E0);
+    const uintptr_t FireProximityPrompt    = REBASE(0x1D710E0);
+    
     namespace InternalFastFlags {
 
-        const uintptr_t EnableLoadModule = REBASE(0x5FA3A60);
-        const uintptr_t DebugCheckRenderThreading = REBASE(0x5FCC168);
-        const uintptr_t RenderDebugCheckThreading2 = REBASE(0x5FF65F8);
-        const uintptr_t DisableCorescriptLoadstring = REBASE(0x5FA3A40);
-        const uintptr_t LockViolationInstanceCrash = REBASE(0x5FAFDC8);
-        const uintptr_t LuaStepIntervalMsOverrideEnabled = REBASE(0x5FA62E8);
-    }
+    const uintptr_t EnableLoadModule                = REBASE(0x6007F08);
+    const uintptr_t DebugCheckRenderThreading       = REBASE(0x6030E68);
+    const uintptr_t RenderDebugCheckThreading2      = REBASE(0x605B230);
+    const uintptr_t DisableCorescriptLoadstring     = REBASE(0x6007EE8);
+    const uintptr_t LockViolationInstanceCrash      = REBASE(0x6014620);
+    const uintptr_t LuaStepIntervalMsOverrideEnabled = REBASE(0x600A700);
+
+}
+
 
 
 
@@ -65,83 +68,82 @@ namespace Offsets {
     }
 
     namespace ReplicateSignal {
-        const uintptr_t isgameloadd = REBASE(0x69DB80);
-        const uintptr_t SetFastFlag = REBASE(0x3827020);
-        const uintptr_t GetFastFlag = REBASE(0x3826590);
-        const uintptr_t Register = REBASE(0x2736350);
-        const uintptr_t CastArgs = REBASE(0xBD8950);
-        const uintptr_t VariantCastInt64 = REBASE(0x14F40B0);
-        const uintptr_t VariantCastInt = REBASE(0x14F3DA0);
-        const uintptr_t VariantCastFloat = REBASE(0x14F46F0);
-    }
+        const uintptr_t isgameloadd         = REBASE(0x69B300);
+        const uintptr_t SetFastFlag         = REBASE(0x3873090);
+        const uintptr_t GetFastFlag         = REBASE(0x3872600);
+        const uintptr_t Register            = REBASE(0x1F119A0);
+        const uintptr_t CastArgs            = REBASE(0xBE0D60);
+        const uintptr_t VariantCastInt64    = REBASE(0x1526730);
+        const uintptr_t VariantCastInt      = REBASE(0x1526420);
+        const uintptr_t VariantCastFloat    = REBASE(0x1526D70);
+}
+
 
 
     namespace Instance {
-        const uintptr_t ClassDescriptor = 0x18;
-        const uintptr_t PropertyDescriptor = 0x3B8;
-        const uintptr_t ClassName = 0x8;
-        const uintptr_t Name = 0x78;
-        const uintptr_t Children = 0x80;
+    const uintptr_t ClassDescriptor     = 0x18;
+    const uintptr_t PropertyDescriptor  = 0x3B8;
+    const uintptr_t ClassName           = 0x8;
+    const uintptr_t Name                = 0x78;
+    const uintptr_t Children            = 0x80;
     }
 
-    namespace Scripts {
-        const uintptr_t LocalScriptEmbedded = 0x1B0;
-        const uintptr_t ModuleScriptEmbedded = 0x158;
-        const uintptr_t weak_thread_node = 0x188;
-        const uintptr_t weak_thread_ref = 0x8;
-        const uintptr_t weak_thread_ref_live = 0x20;
-        const uintptr_t weak_thread_ref_live_thread = 0x8;
-    }
+namespace Scripts {
+    const uintptr_t LocalScriptEmbedded           = 0x1B0;
+    const uintptr_t ModuleScriptEmbedded          = 0x158;
+    const uintptr_t weak_thread_node              = 0x188;
+    const uintptr_t weak_thread_ref               = 0x8;
+    const uintptr_t weak_thread_ref_live          = 0x20;
+    const uintptr_t weak_thread_ref_live_thread   = 0x8;
+}
 
-    namespace ExtraSpace {
-        const uintptr_t Identity = 0x30;
-        const uintptr_t Capabilities = 0x48;
-    }
+namespace ExtraSpace {
+    const uintptr_t Identity      = 0x30;
+    const uintptr_t Capabilities = 0x48;
+}
 
-    namespace TaskScheduler {
-        const uintptr_t FpsCap = 0x1B0;
-        const uintptr_t JobStart = 0x1D0;
-        const uintptr_t JobEnd = 0x1D8;
+namespace TaskScheduler {
+    const uintptr_t FpsCap   = 0x1B0;
+    const uintptr_t JobStart = 0x1D0;
+    const uintptr_t JobEnd   = 0x1D8;
 
-        namespace Job {
-            const uintptr_t Name = 0x18;
-            const uintptr_t TypeName = 0x150;
-            namespace WaitingHybridScriptsJob {
-                const uintptr_t ScriptContext = 0x1F8;
-            }
+    namespace Job {
+        const uintptr_t Name     = 0x18;
+        const uintptr_t TypeName = 0x150;
+        namespace WaitingHybridScriptsJob {
+            const uintptr_t ScriptContext = 0x1F8;
         }
     }
+}
 
-    namespace DataModel
-    {
-        constexpr uintptr_t PropertiesStart = 0x30;
-        constexpr uintptr_t PropertiesEnd = 0x38;
-        constexpr uintptr_t Type = 0x60;
-        constexpr uintptr_t TypeGetSetDescriptor = 0x98;
-        constexpr uintptr_t getVFtableFunc = 0x10;
-        const     uintptr_t FakeDataModelPointer = REBASE(0x675AA38);
-        constexpr uintptr_t FakeDataModelToDataModel = 0x1B8;
+namespace DataModel {
+    constexpr uintptr_t PropertiesStart           = 0x30;
+    constexpr uintptr_t PropertiesEnd             = 0x38;
+    constexpr uintptr_t Type                      = 0x60;
+    constexpr uintptr_t TypeGetSetDescriptor      = 0x98;
+    constexpr uintptr_t getVFtableFunc            = 0x10;
+    const     uintptr_t FakeDataModelPointer      = REBASE(0x67C2958);
+    constexpr uintptr_t FakeDataModelToDataModel  = 0x1B8;
 
-        constexpr uintptr_t GameLoaded = 0x650;
-        constexpr uintptr_t PlaceId = 0x1A0;
-        constexpr uintptr_t GameId = 0x198;
-        constexpr uintptr_t ModuleFlags = 0x6E0 - 0x4;
-        constexpr uintptr_t IsCoreScript = 0x6E0;
-        constexpr uintptr_t Children = 0x80;
-        constexpr uintptr_t ChildrenEnd = 0x8;
+    constexpr uintptr_t GameLoaded                = 0x650;
+    constexpr uintptr_t PlaceId                   = 0x1A0;
+    constexpr uintptr_t GameId                    = 0x198;
+    constexpr uintptr_t ModuleFlags               = 0x6E0 - 0x4;
+    constexpr uintptr_t IsCoreScript              = 0x6E0;
+    constexpr uintptr_t Children                  = 0x80;
+    constexpr uintptr_t ChildrenEnd               = 0x8;
 
-        constexpr uintptr_t Name = 0x78;
-        constexpr uintptr_t ClassDescriptor = 0x18;
-        constexpr uintptr_t PropDescriptor = 0x3B8;
-        constexpr uintptr_t ClassName = 0x8;
-        constexpr uintptr_t PrimitiveTouch = 0x178LL;
-        constexpr uintptr_t Overlap = 0x1d0;
+    constexpr uintptr_t Name                      = 0x78;
+    constexpr uintptr_t ClassDescriptor           = 0x18;
+    constexpr uintptr_t PropDescriptor            = 0x3B8;
+    constexpr uintptr_t ClassName                 = 0x8;
+    constexpr uintptr_t PrimitiveTouch            = 0x178LL;
+    constexpr uintptr_t Overlap                   = 0x1D0;
 
-        const uintptr_t weak_thread_node = 0x188;
-        const uintptr_t weak_thread_ref = 0x8;
-        const uintptr_t weak_thread_ref_live = 0x20;
-        const uintptr_t weak_thread_ref_live_thread = 0x8;
-    }
+    const uintptr_t weak_thread_node              = 0x188;
+    const uintptr_t weak_thread_ref               = 0x8;
+    const uintptr_t weak_thread_ref_live          = 0x20;
+    const uintptr_t weak_thread_ref_live_thread   = 0x8;
 }
 
 
